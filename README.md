c2922a67ccbeff8e74dc03e4d4777484f30777bf
Alena Domanina <alena.domanina@seven.one>
2023-08-18 12:12:36 +0200

    added cli options

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # fundraiseup_job_interview_task
+---
+Pytest+python+playwright
+---
+Before running (locally)
+---
+`pip3 install -r requirements.txt`
+
+`playwright install`
+
+`playwright install-deps`
+
+---
+Run tests default (chromium, desktop, headless false)
+---
+`pytest -v`
+---
+
+---
+Run tests with diferent browser/device/headless
+---
+
+`pytest -v --browser-choice firefox/cromium/webkit --device desktop/iphone --headless` 
+
+Run tests parallel
+---
+`pytest -n number_of_workers`
+
+Allure
+---
+
+run with Allure reports:
+
+`pytest -v --alluredir=allure-results`
+
+start Allure:
+
+`allure serve allure-results`
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,21 +2,33 @@ import allure
 import pytest
 from playwright.sync_api import sync_playwright
 
-from config.config import HEADLESS_MODE, RUN_BROWSER
+
+def pytest_addoption(parser):
+    parser.addoption("--browser-choice", choices=["webkit", "chromium", "firefox"], default="chromium", help="Browser choice")
+    parser.addoption("--device-type", choices=["desktop", "iphone"], default="desktop", help="Device type")
+    parser.addoption("--headless", action="store_true", help="Run in headless mode")
 
 
 @allure.title("Open browser incognito")
 @pytest.fixture(scope="function")
-def incognito():
+def incognito(request):
+    browser_choice = request.config.getoption("--browser-choice")
+    device_type = request.config.getoption("--device-type")
+    headless_mode = request.config.getoption("--headless")
+
     with sync_playwright() as playwright:
-        headless = False if HEADLESS_MODE else True
-        if RUN_BROWSER == "webkit":
-            browser = playwright.webkit.launch(headless=headless)
-        elif RUN_BROWSER == "chromium":
-            browser = playwright.chromium.launch(headless=headless)
-        elif RUN_BROWSER == "firefox":
-            browser = playwright.firefox.launch(headless=headless)
-        context = browser.new_context()
+        if browser_choice == "webkit":
+            browser = playwright.webkit.launch(headless=headless_mode)
+        elif browser_choice == "chromium":
+            browser = playwright.chromium.launch(headless=headless_mode)
+        elif browser_choice == "firefox":
+            browser = playwright.firefox.launch(headless=headless_mode)
+
+        if device_type == "desktop":
+            context = browser.new_context()
+        elif device_type == "iphone":
+            iphone_13 = playwright.devices['iPhone 13']
+            context = browser.new_context(**iphone_13)
         page = context.new_page()
         yield page
         browser.close()
